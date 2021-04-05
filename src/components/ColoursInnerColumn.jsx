0020d9f79ee7e4f3cda347c4da0c6f0313e2356f
Derek Price <derekp098@gmail.com>
2021-04-04 17:25:36 -0700

    added tooltips and organized some divs

--- src/components/ColoursInnerColumn.jsx
+++ src/components/ColoursInnerColumn.jsx
@@ -4,63 +4,73 @@ import {removeColour, getLightnessFromHex, handleChangeColour, HexToRGB, handleS
 const ColoursInnerColumn = ({colours, setColourArray, colourObj}) => {
 
   return (
-    <div>
       <div className='colour-column-inner' >
-        <button
-          className='
+        <div className='colour-column-inner-btns'>
+          <button
+            className='
+              colour-column-item
+              clickable
+              tooltip
+              btn-delete
+            '
+            onClick={() => removeColour(colourObj.pos, colours, setColourArray)}
+            style={{color: getLightnessFromHex(colourObj.colour) > 50 ? 'black' : 'white'}}
+          >
+            X
+            <span className='tooltiptext' >Remove Colour</span>
+          </button>
+          <button
+            className='
+              colour-column-item
+              clickable
+              tooltip
+              btn-shades
+            '
+            onClick={() => handleShadeChange(colours, colourObj, setColourArray)}
+            style={{color: getLightnessFromHex(colourObj.colour) > 50 ? 'black' : 'white'}}
+          >
+            <p className="iconify" data-icon="heroicons-outline:view-list" data-inline="false"></p>
+            <span className='tooltiptext' >Colour Shades</span>
+          </button>
+          <div
+            className='
             colour-column-item
             clickable
-            btn-delete
-          '
-          onClick={() => removeColour(colourObj.pos, colours, setColourArray)}
-          style={{color: getLightnessFromHex(colourObj.colour) > 50 ? 'black' : 'white'}}
-        >
-          X
-        </button>
-
-        <button
-          className='
-            colour-column-item
-            clickable
-            btn-shades
-          '
-          onClick={() => handleShadeChange(colours, colourObj, setColourArray)}
-          style={{color: getLightnessFromHex(colourObj.colour) > 50 ? 'black' : 'white'}}
-        >
-          <p className="iconify" data-icon="heroicons-outline:view-list" data-inline="false"></p>
-        </button>
-
-        <input
-          className='
-            colour-column-item
-            clickable
+            tooltip
             colour-column-item-input
           '
-          type="color" value={colourObj.colour}
-          onChange={(e) => {handleChangeColour(e, colourObj.pos, colours,  setColourArray)}}
-        />
-        <ColourValueText
-          className='
-            colour-column-item
-            clickable
-            tooltip
-            hex-text
-          '
-          colour={colourObj.colour}
-          defaultText='COPY HEX'
-        />
-        < ColourValueText
-          className='
-            colour-column-item
-            clickable
-            tooltip
-            rgb-text
-          '
-          colour={HexToRGB(colourObj.colour)}
-          defaultText='COPY RGB'
-        />
+          >
+            <input
+              type="color" 
+              value={colourObj.colour}
+              onChange={(e) => {handleChangeColour(e, colourObj.pos, colours,  setColourArray)}}
+            />
+            <span className='tooltiptext' >Change Colour</span>
+          </div>
+        </div>
+        <div className='colour-column-inner-text'>
+          <ColourValueText
+            className='
+              colour-column-item
+              clickable
+              tooltip
+              hex-text
+            '
+            colour={colourObj.colour}
+            defaultText='COPY HEX'
+          />
+          < ColourValueText
+            className='
+              colour-column-item
+              clickable
+              tooltip
+              rgb-text
+            '
+            colour={HexToRGB(colourObj.colour)}
+            defaultText='COPY RGB'
+          />
+        </div>
       </div>
-    </div>
   )
 }
 
